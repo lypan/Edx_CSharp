94f77037ac9475c6c450363d9b34cd9d1a1a8c16
lypan <xxzero12@gmail.com>
2015-04-30 17:43:06 +0800

    Hw2

---
+++ Edx_CSharp_Hw2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 7; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        if (col % 2 == 0)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            Console.Write("O");
+                        }
+                    }
+                    else
+                    {
+                        if (col % 2 == 0)
+                        {
+                            Console.Write("O");
+                        }
+                        else
+                        {
+                            Console.Write("X");
+                        }
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
